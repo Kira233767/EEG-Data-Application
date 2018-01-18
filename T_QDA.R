568560112a0e889e37f95e961b66ac4b215639c9
Li <peter@Lis-MacBook-Pro.local>
2018-01-18 17:17:09 -0500

    Core Code
    Time Varying QDA, core code

---
+++ T_QDA.R
@@ -0,0 +1,15 @@
+# Time Varying Quadratic Discriminant Analysis
+
+# EEG Data Application
+
+# install.packages("glasso", dependencies = T)
+library(glasso)
+library(MASS)
+
+# Kernel Function, using Gaussian Kernel
+GetKernel <- function(x, y, h){
+  Kernelvalue <- exp(-(sum((x - y)^2)) / (2*h))
+  return(Kernelvalue)
+}
+
+
